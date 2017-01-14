b6e80f8ee4299490053bcc5bde9581a51ae048b0
Mykaelos <mmaderii@gmail.com>
2017-01-14 16:33:14 -0600

    Added the sun. It rotates randomly to kind of give it a twinkle. Not sure if I'm going to keep it, but it works.

---
+++ Assets/Scripts/SunController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour {
+    Timer RotationTimer;
+    public float RotationDelay = 1;
+
+
+    void Awake() {
+        RotationTimer = new Timer();
+    }
+
+    void Update() {
+        if (RotationTimer.Check(RotationDelay)) {
+            RotationTimer.Reset();
+
+            transform.Rotate(Vector3.forward, Random.Range(0f, 360f));
+        }
+    }
+}
